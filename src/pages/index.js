6adae3c6b2fb7294cb4418c1842279b878e9aad0
0xdagarn <sgnoo@naver.com>
2023-04-15 18:52:54 +0900

    Update main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,20 +23,31 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col content-center justify-center items-center">
+      <div className="text-white text-5xl my-20">InviteApe</div>
       <button
+        className="bg-green-700 w-85 h-48 rounded-3xl text-2xl text-white border-2 border-transparent hover:border-green-500"
+        style={{
+          width: "349px",
+          height: "190px",
+        }}
         onClick={() => {
           connectWalletRedirectToCreateEvent();
         }}
       >
-        Create
+        Create Event
       </button>
       <button
+        className="mt-4 bg-orange-600 w-85 h-48 rounded-3xl text-2xl text-white border-2 border-transparent hover:border-yellow-500"
+        style={{
+          width: "349px",
+          height: "190px",
+        }}
         onClick={() => {
           connectWalletRedirectToBrowseEvent();
         }}
       >
-        Browse
+        Browse Event
       </button>
     </div>
   );
