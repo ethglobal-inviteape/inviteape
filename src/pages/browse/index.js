3ca12210edff2b3a14ce4300391b088613cc48bc
0xdagarn <sgnoo@naver.com>
2023-04-15 13:52:35 +0900

    Create event and get list of events

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react";
+import Router from "next/router";
+
 export default function Browse() {
-  return <div>browse</div>;
+  const [events, setEvents] = useState(null);
+
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("/api/get");
+      const events = await response.json();
+
+      setEvents(events);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  return (
+    <div>
+      {events &&
+        events.map((event, index) => (
+          <div
+            onClick={() => {
+              Router.push("/event/" + event.title);
+            }}
+            key={event.title}
+          >
+            <div>{event.title}</div>
+            <div>{event.childERC721}</div>
+            <div>{event.motherERC721}</div>
+          </div>
+        ))}
+    </div>
+  );
 }
--- src/pages/create/index.js
+++ src/pages/create/index.js
@@ -5,7 +5,12 @@ import ChildERC721 from "../../abi/ChildERC721.json";
 export default function Create() {
   const { data: signer } = useSigner();
 
-  const deploy = async () => {
+  const deployAndAddEvent = async (title) => {
+    const contract = await _deploy();
+    _addEvent(title, contract.address);
+  };
+
+  const _deploy = async () => {
     const factory = new ContractFactory(
       ChildERC721.abi,
       ChildERC721.bytecode,
@@ -16,12 +21,15 @@ export default function Create() {
       "cTKN",
       "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
     );
-    console.log(contract);
+    return contract;
   };
 
-  const addEvent = async () => {
+  const _addEvent = async (title, address) => {
+    const motherERC721 = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
     const event = {
-      title: "1234",
+      title,
+      childERC721: address,
+      motherERC721,
     };
     try {
       const response = await fetch("/api/add", {
@@ -32,8 +40,7 @@ export default function Create() {
         body: JSON.stringify(event),
       });
 
-      const data = await response.json();
-      console.log("Item added:", data);
+      await response.json();
     } catch (error) {
       console.error("Error adding item:", error);
     }
@@ -41,7 +48,7 @@ export default function Create() {
 
   return (
     <div>
-      <button onClick={() => addEvent()}>Deploy</button>
+      <button onClick={() => deployAndAddEvent("title")}>Deploy</button>
     </div>
   );
 }
