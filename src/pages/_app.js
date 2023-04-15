3016a8022f19df70ca41b6b8142565f7ada66778
0xdagarn <sgnoo@naver.com>
2023-04-16 01:50:18 +0900

    Add margins to wallet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ import { mainnet, goerli, hardhat } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const { chains, provider } = configureChains(
   [mainnet, goerli, hardhat],
@@ -32,11 +32,10 @@ export default function App({ Component, pageProps }) {
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <QueryClientProvider client={queryClient}>
-          <div className="flex flex-row-reverse mr-6">
+          <div className="flex flex-row-reverse mr-6 mt-4">
             <ConnectButton />
           </div>
-          <div
-            className="-mt-[40px] w-96 mx-auto px-8 min-h-screen flex flex-col shadow-md overflow-y-auto overflow-x-auto border-black bg-[#211D1D] text-white">
+          <div className="-mt-[40px] w-96 mx-auto px-8 min-h-screen flex flex-col shadow-md overflow-y-auto overflow-x-auto border-black bg-[#211D1D] text-white">
             <Component {...pageProps} />
           </div>
         </QueryClientProvider>
