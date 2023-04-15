3f915a60ed2aa1886397fd364b97d356a6901f92
0xdagarn <sgnoo@naver.com>
2023-04-15 12:24:09 +0900

    Deploy ChildERC721 tokens

--- src/pages/create/index.js
+++ src/pages/create/index.js
@@ -1,3 +1,27 @@
+import { useSigner } from "wagmi";
+import { ContractFactory } from "ethers";
+import ChildERC721 from "../../abi/ChildERC721.json";
+
 export default function Create() {
-  return <div>create</div>;
+  const { data: signer } = useSigner();
+
+  const deploy = async () => {
+    const factory = new ContractFactory(
+      ChildERC721.abi,
+      ChildERC721.bytecode,
+      signer
+    );
+    const contract = await factory.deploy(
+      "ChildERC721",
+      "cTKN",
+      "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    );
+    console.log(contract);
+  };
+
+  return (
+    <div>
+      <button onClick={() => deploy()}>Deploy</button>
+    </div>
+  );
 }
